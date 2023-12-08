9940d3f8663aa68929d4b7157bc3f59d961ff067
NIkittansk <61756114+Nikittansk@users.noreply.github.com>
2023-12-08 20:38:17 +0300

    Router created, user handler registered

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -1,5 +1,31 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"rest-api-tutorial/internal/user"
+	"time"
 
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+	log.Println("Create router")
+	router := httprouter.New()
+
+	log.Println("Register user handler")
+	handler := user.NewHandler()
+	handler.Register(router)
+	start(router)
+}
+
+func start(router *httprouter.Router) {
+	server := &http.Server{
+		Addr:         ":8080",
+		Handler:      router,
+		ReadTimeout:  15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+	}
+	log.Println("The server is running and listening on port: 8080")
+	log.Fatal(server.ListenAndServe())
 }
