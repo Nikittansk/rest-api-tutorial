b0383250d93722d9207287124f05a791a5b4bb50
NIkittansk <61756114+Nikittansk@users.noreply.github.com>
2023-12-09 20:15:26 +0300

    Added logger

--- cmd/main/main.go
+++ cmd/main/main.go
@@ -3,17 +3,29 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 	"rest-api-tutorial/internal/user"
 	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+var infoLog *log.Logger
+var errorLog *log.Logger
+
 func main() {
-	log.Println("Create router")
+	// Write log
+	var file, err = os.OpenFile("/log/all.log", os.O_RDWR|os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	infoLog = log.New(file, "INFO\t", log.Ldate|log.Ltime)
+	errorLog = log.New(file, "ERROR\t", log.Ldate|log.Ltime|log.Llongfile)
+
+	infoLog.Println("Create router")
 	router := httprouter.New()
 
-	log.Println("Register user handler")
+	infoLog.Println("Register user handler")
 	handler := user.NewHandler()
 	handler.Register(router)
 	start(router)
@@ -23,9 +35,10 @@ func start(router *httprouter.Router) {
 	server := &http.Server{
 		Addr:         ":8080",
 		Handler:      router,
+		ErrorLog:     errorLog,
 		ReadTimeout:  15 * time.Second,
 		WriteTimeout: 15 * time.Second,
 	}
-	log.Println("The server is running and listening on port: 8080")
-	log.Fatal(server.ListenAndServe())
+	infoLog.Println("The server is running and listening on port: 8080")
+	errorLog.Fatal(server.ListenAndServe())
 }
